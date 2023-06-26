178f7c69b9eb7fa77deeda64317a1647edca2219
tubi55 <tubi55@nate.com>
2023-06-26 13:14:46 +0900

    기본 라우터 이동 메뉴 설정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,34 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/department'>Department</NavLink>
+				</li>
+				<li>
+					<NavLink to='/community'>Community</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery'>Gallery</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube'>Youtube</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact'>Contact</NavLink>
+				</li>
+				<li>
+					<NavLink to='/member'>Member</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
