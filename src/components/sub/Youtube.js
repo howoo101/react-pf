11586193d4f55326de10f27280ced56b2075a8be
ho <hohohosky101@gmail.com>
2023-06-28 01:26:24 +0900

    axios promise -> async await

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Youtube() {
-	const apiKey = 'AIzaSyBm1-5iAqRnlxETXyLSvDYAaSnMKGrr8fY';
-	const playlistId = 'PLtyGCdgf6inmUrDz2XNQJq37nfcZFOJ_M';
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50`;
+	async function fetchData() {
+		const apiKey = 'AIzaSyBm1-5iAqRnlxETXyLSvDYAaSnMKGrr8fY';
+		const playlistId = 'PLtyGCdgf6inmUrDz2XNQJq37nfcZFOJ_M';
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50`;
+		const data = await axios.get(url);
+		console.log(data);
+		const json = await data.data.items;
+		setVideoes(json);
+	}
 
 	const [Videoes, setVideoes] = useState([]);
 	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.items);
-			setVideoes(json.data.items);
-		});
+		fetchData();
 	}, []);
 
 	return (
@@ -20,8 +23,12 @@ function Youtube() {
 			{Videoes.map((video, idx) => {
 				const data = video.snippet;
 				const thumbnailUrl = data.thumbnails.standard.url;
-				const title = data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
-				const description = data.description.length > 200 ? data.description.substr(0, 200) + '...' : data.description;
+				const title =
+					data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
+				const description =
+					data.description.length > 200
+						? data.description.substr(0, 200) + '...'
+						: data.description;
 
 				return (
 					<article key={idx}>
