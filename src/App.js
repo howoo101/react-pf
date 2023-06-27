316f5d012dcc8037b82a3395360856c870dc8b83
tubi55 <tubi55@nate.com>
2023-06-27 09:42:02 +0900

    기존 scss연결

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import Gallery from './components/sub/Gallery';
 import Member from './components/sub/Member';
 import Youtube from './components/sub/Youtube';
 
+import './scss/style.scss';
+
 function App() {
 	return (
 		<>
