63615a0102f8f2520695b6f7ea895e6a392dc532
homean <hohohosky101@gmail.com>
2023-06-28 12:11:49 +0900

    유튜브 썸네일 클릭시 모달창으로 유튜브 동영상

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,45 +1,65 @@
 import Layout from '../common/Layout';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-
+import Modal from '../common/Modal';
 function Youtube() {
-	async function fetchData() {
+	const fetchYoutube = async () => {
 		const apiKey = 'AIzaSyBm1-5iAqRnlxETXyLSvDYAaSnMKGrr8fY';
 		const playlistId = 'PLtyGCdgf6inmUrDz2XNQJq37nfcZFOJ_M';
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50`;
 		const data = await axios.get(url);
-		console.log(data);
+		// console.log(data);
 		const json = await data.data.items;
 		setVideoes(json);
-	}
-
+	};
+	const modal = useRef(null);
 	const [Videoes, setVideoes] = useState([]);
+	const [idx, setIdx] = useState(0);
 	useEffect(() => {
-		fetchData();
+		fetchYoutube();
 	}, []);
 
 	return (
-		<Layout name={'Youtube'}>
-			{Videoes.map((video, idx) => {
-				const data = video.snippet;
-				const thumbnailUrl = data.thumbnails.standard.url;
-				const title = data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
-				const description = data.description.length > 200 ? data.description.substr(0, 200) + '...' : data.description;
+		<>
+			<Layout name={'Youtube'}>
+				{Videoes.map((video, idx) => {
+					const data = video.snippet;
+					const thumbnailUrl = data.thumbnails.standard.url;
+					const title = data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
+					const description =
+						data.description.length > 200 ? data.description.substr(0, 200) + '...' : data.description;
 
-				return (
-					<article key={idx}>
-						<h2>{title}</h2>
-						<div className='txt'>
-							<p>{description}</p>
-							<span>{data.publishedAt.split('T')[0]}</span>
-						</div>
-						<div className='pic'>
-							<img src={thumbnailUrl} alt={thumbnailUrl} />
-						</div>
-					</article>
-				);
-			})}
-		</Layout>
+					return (
+						<article key={idx}>
+							<h2>{title}</h2>
+							<div className='txt'>
+								<p>{description}</p>
+								<span>{data.publishedAt.split('T')[0]}</span>
+							</div>
+							<div
+								className='pic'
+								onClick={() => {
+									modal.current.open();
+									setIdx(idx);
+								}}
+							>
+								<img src={thumbnailUrl} alt={thumbnailUrl} />
+							</div>
+						</article>
+					);
+				})}
+			</Layout>
+
+			<Modal ref={modal}>
+				{Videoes.length > 0 && (
+					<iframe
+						title={Videoes[idx].id}
+						src={`https://www.youtube.com/embed/${Videoes[idx].snippet.resourceId.videoId}`}
+						frameborder='0'
+					></iframe>
+				)}
+			</Modal>
+		</>
 	);
 }
 
