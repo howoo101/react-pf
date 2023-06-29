6da568e6e01684c0226b5198862f0c92c01128ce
homean <hohohosky101@gmail.com>
2023-06-29 12:16:00 +0900

    base값까지 적용된 커스텀 스크롤 모션

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,7 +1,20 @@
 function Pics({ scroll, pos }) {
+	const currentPos = scroll - pos;
+	const base = window.innerHeight / 2;
+	const modified = currentPos + base;
+
 	return (
 		<section id='pics' className='myScroll'>
-			<h1 style={{ transform: `translateX(${scroll - pos}px)` }}>FLICKR</h1>
+			<h1 style={{ transform: `translateX(${currentPos}px)` }}>FLICKR</h1>
+
+			<article
+				style={{
+					transform: `translate(-50%, -50%) rotate(${scroll >= pos - base ? modified : 0}deg) scale(${
+						scroll >= pos - base ? 1 + modified / 500 : 1
+					}) `,
+					opacity: `${scroll >= pos - base ? 1 - modified / 500 : 1}`,
+				}}
+			></article>
 		</section>
 	);
 }
