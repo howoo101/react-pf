903aa903ec3eb29f04146c3160f8ebd1104f2cb4
homean <hohohosky101@gmail.com>
2023-06-29 12:59:00 +0900

    리액트 버전에 맞게 카카오 지도 인스턴스 생성

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,7 +1,32 @@
+import { useEffect, useRef } from 'react';
 import Layout from '../common/Layout';
 
 function Contact() {
-	return <Layout name={'Contact'}></Layout>;
+	const container = useRef(null);
+	const { kakao } = window;
+
+	const option = {
+		// api 사용할때 못찾는다고할때 비구조할당으로 window를 할당해라
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		// 인스턴스 호출 구문은 마운트시 호출
+		const map = new kakao.maps.Map(container.current, option);
+		var marker = new kakao.maps.Marker({
+			position: option.center,
+		});
+
+		// 마커가 지도 위에 표시되도록 설정합니다
+		marker.setMap(map);
+	}, []);
+
+	return (
+		<Layout name={'Contact'}>
+			<div ref={container} id='map'></div>
+		</Layout>
+	);
 }
 
 export default Contact;
