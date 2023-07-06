8bd918d26bfbc153cdf185e7fd78ef68eff867b7
homean <hohohosky101@gmail.com>
2023-07-06 10:17:12 +0900

    모든이미지 소스가 로딩완료되고 masonry가 UI배치를 해줄떄까지 로딩 이미지 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,9 +1,15 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
 import Masonry from 'react-masonry-component';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const [Items, setItems] = useState([]);
+	const [Loader, setLoader] = useState(true);
+
+	const frame = useRef(null);
+	const counter = useRef(0);
+
 	const getFlickr = async (opt) => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = '287211516f841c2ab9b7a6101334112c';
@@ -11,7 +17,7 @@ function Gallery() {
 		const method_search = 'flickr.photos.search';
 		const method_user = 'flickr.people.getPhotos';
 		const userId = '198477162@N05';
-		const num = 20;
+		const num = 500;
 		let url = '';
 		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		if (opt.type === 'search') url = `${baseURL}${method_search}&tags=${opt.value}`;
@@ -20,15 +26,31 @@ function Gallery() {
 		const result = await axios.get(url);
 		console.log(result);
 		setItems(result.data.photos.photo);
+		//외부데이터가 State에 담기고 DOM이 생성되는 순간
+		//모든 img요소를 찾아서 반복처리
+		const imgs = frame.current.querySelectorAll('img');
+		counter.current = 0;
+		imgs.forEach((img) => {
+			//이미지요소에 load이벤트가 발생할때 (소스이미지까지 로딩이 완료될떄마다)
+			img.onload = () => {
+				//내부적으로 카운터값을 1씩 증가
+				++counter.current;
+				console.log(counter);
+				//로딩완료된 이미지수와 전체이미지수가 같아지면
+				if (counter.current === imgs.length) {
+					//로더 제거하고 이미지 갤러리 보임처리
+					setLoader(false);
+					frame.current.classList.add('on');
+				}
+			};
+		});
 	};
-
-	const [Items, setItems] = useState([]);
 
 	useEffect(() => getFlickr({ type: 'interest' }), []);
 
 	return (
 		<Layout name={'Gallery'}>
-			<div className='frame'>
+			<div className='frame' ref={frame}>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
 					{Items.map((item, idx) => {
 						return (
@@ -41,10 +63,11 @@ function Gallery() {
 										/>
 									</div>
 									<h2>{item.title}</h2>
-									<div class='profile'>
+									<div className='profile'>
 										<img
 											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
 											alt={item.title}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
 										/>
 										<span>{item.owner}</span>
 									</div>
@@ -54,6 +77,7 @@ function Gallery() {
 					})}
 				</Masonry>
 			</div>
+			{Loader && <img className='loader' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loader' />}
 		</Layout>
 	);
 }
