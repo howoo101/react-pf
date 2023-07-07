2d5760a48cb40fb18df284a30ea5b23f494b747d
homean <hohohosky101@gmail.com>
2023-07-07 12:41:56 +0900

    모바일 메뉴 이동

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,17 +1,61 @@
 import React, { forwardRef, useImperativeHandle, useState } from 'react';
-
+import { motion, AnimatePresence } from 'framer-motion';
+import { Link, NavLink } from 'react-router-dom';
 const Menu = forwardRef((props, ref) => {
+	const active = { color: 'aqua' };
 	const [Open, setOpen] = useState(false);
 	useImperativeHandle(ref, () => {
 		return { toggle: () => setOpen(!Open) };
 	});
 	return (
 		<>
-			{Open && (
-				<nav id='mobilePanel'>
-					<h1>LOGO</h1>
-				</nav>
-			)}
+			<AnimatePresence>
+				{Open && (
+					<motion.nav
+						id='mobilePanel'
+						initial={{ opacity: 0, x: -280 }}
+						animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+						exit={{ opacity: 0, x: -280, transition: { duration: 0.5 } }}
+					>
+						<h1>
+							<Link to='/'>LOGO</Link>
+						</h1>
+
+						<ul id='gnbMo'>
+							<li>
+								<NavLink to='/department' activeStyle={active}>
+									Department
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/community' activeStyle={active}>
+									Community
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/gallery' activeStyle={active}>
+									Gallery
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/youtube' activeStyle={active}>
+									Youtube
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/contact' activeStyle={active}>
+									Contact
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/member' activeStyle={active}>
+									Members
+								</NavLink>
+							</li>
+						</ul>
+					</motion.nav>
+				)}
+			</AnimatePresence>
 		</>
 	);
 });
