bf26e649c272a652b445de0ab5902c7d65379aee
homean <hohohosky101@gmail.com>
2023-07-11 12:51:48 +0900

    action객체를 받아서 기존의 맴버데이터를 변경해서 리턴해주는 reducer함수 생성

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,1 +1,51 @@
+import { combineReducers } from 'redux';
+
 //store의 데이터를 변경해주는 변형자함수
+const initMember = {
+	members: [
+		{
+			name: 'Julia',
+			position: 'President',
+			pic: 'member1.jpg',
+		},
+		{
+			name: 'David',
+			position: 'Vice President',
+			pic: 'member2.jpg',
+		},
+		{
+			name: 'Emily',
+			position: 'UI Designer',
+			pic: 'member3.jpg',
+		},
+		{
+			name: 'Paul',
+			position: 'Front-end Engineer',
+			pic: 'member4.jpg',
+		},
+		{
+			name: 'Sara',
+			position: 'Back-end Engineer',
+			pic: 'member5.jpg',
+		},
+		{
+			name: 'Michael',
+			position: 'Project Manager',
+			pic: 'member6.jpg',
+		},
+	],
+};
+
+// 초기 데이터값을 state로 지정하고 action객체가 넘어오면 타입에따라 데이터를 변경해주는 변형자 함수
+const memberReducer = (state = initMember, action) => {
+	switch (action.type) {
+		case 'SET_MEMBERS':
+			return { ...state, members: action.payload };
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ memberReducer });
+
+export default reducers;
