f8662db7111d1e790bb924affe356a019ca60ddd
homean <hohohosky101@gmail.com>
2023-07-13 13:05:24 +0900

    메인의 Vids컴포넌트에서 전역 유튜브 데이터 호출

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,6 +1,8 @@
 import { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 function Vids() {
+	const Vids = useSelector((store) => store.youtubeReducer.youtube);
 	return (
 		<section id='vids' className='myScroll'>
 			Vids
