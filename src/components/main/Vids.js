7cd35a22285382516a38257179e23ff395738019
homean <hohohosky101@gmail.com>
2023-07-17 10:11:31 +0900

    메인 컴포넌트 유튜브 미리보기 출력

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,9 +1,22 @@
 import { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 function Vids() {
+	const Vids = useSelector((store) => store.youtube.data);
 	return (
 		<section id='vids' className='myScroll'>
-			Vids
+			{Vids.map((vid, idx) => {
+				if (idx >= 5) return null;
+
+				return (
+					<article>
+						<div className='pic'>
+							<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+						</div>
+					</article>
+				);
+			})}
+			;
 		</section>
 	);
 }
