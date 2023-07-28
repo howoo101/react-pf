38a009e759f43194b13d5c592edceed3dd0314f9
homean <hohohosky101@gmail.com>
2023-07-28 13:03:55 +0900

    youtube컴포넌트 react-query처리

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -2,48 +2,49 @@ import Layout from '../common/Layout';
 import { useState, useRef } from 'react';
 
 import Modal from '../common/Modal';
-import { useSelector } from 'react-redux';
+import { useYoutubeQuery } from '../../hooks/useYoutuebeQuery';
 function Youtube() {
 	const modal = useRef(null);
-	const Videoes = useSelector((store) => store.youtube.data);
+	const { data: Videoes, isSuccess } = useYoutubeQuery();
 	const [idx, setIdx] = useState(0);
 
 	return (
 		<>
 			<Layout name={'Youtube'}>
-				{Videoes?.map((video, idx) => {
-					const data = video.snippet;
-					const thumbnailUrl = data.thumbnails.standard.url;
-					const title = data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
-					const description =
-						data.description.length > 200 ? data.description.substr(0, 200) + '...' : data.description;
+				{isSuccess &&
+					Videoes?.map((video, idx) => {
+						const data = video.snippet;
+						const thumbnailUrl = data.thumbnails.standard.url;
+						const title = data.title.length > 50 ? data.title.substr(0, 50) + '...' : data.title;
+						const description =
+							data.description.length > 200 ? data.description.substr(0, 200) + '...' : data.description;
 
-					return (
-						<article key={idx}>
-							<h2>{title}</h2>
-							<div className='txt'>
-								<p>{description}</p>
-								<span>{data.publishedAt.split('T')[0]}</span>
-							</div>
-							<div
-								className='pic'
-								onClick={() => {
-									modal.current.open();
-									setIdx(idx);
-								}}
-							>
-								<img src={thumbnailUrl} alt={thumbnailUrl} />
-							</div>
-						</article>
-					);
-				})}
+						return (
+							<article key={idx}>
+								<h2>{title}</h2>
+								<div className='txt'>
+									<p>{description}</p>
+									<span>{data.publishedAt.split('T')[0]}</span>
+								</div>
+								<div
+									className='pic'
+									onClick={() => {
+										modal.current.open();
+										setIdx(idx);
+									}}
+								>
+									<img src={thumbnailUrl} alt={thumbnailUrl} />
+								</div>
+							</article>
+						);
+					})}
 			</Layout>
 
 			<Modal ref={modal}>
-				{Videoes.length > 0 && (
+				{isSuccess && Videoes.length > 0 && (
 					<iframe
-						title={Videoes[idx].id}
-						src={`https://www.youtube.com/embed/${Videoes[idx].snippet.resourceId.videoId}`}
+						title={isSuccess && Videoes[idx].id}
+						src={`https://www.youtube.com/embed/${isSuccess && Videoes[idx].snippet.resourceId.videoId}`}
 						frameborder='0'
 					></iframe>
 				)}
